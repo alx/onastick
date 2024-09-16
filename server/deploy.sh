8003c3f75b7f23f38d999b208bfa1fdab8f903b8
Alexandre Girard Davila <git@alexgirard.com>
2024-09-16 14:49:25 +0700

    fix: working workflow

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-hugo
-git add public
-git commit -m 'build: udpate public'
-git push
-
-ssh root@192.168.8.178 'cd /opt/onastick; git pull'
+hugo && rsync -r ./public/ root@192.168.8.178:/media/mmcblk0p2/www/onastick/
--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,129 @@
-async function getImgBase64(url) {
-    try {
-      const response = await fetch(url);
-      const blob = await response.blob();
-      const bmp = await createImageBitmap(blob);
 
-      var canvas = document.createElement('CANVAS');
-      canvas.width = 800;
-      canvas.height = 600;
+const getImgSource = async (url) => {
 
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(bmp, 0, 0);
-      var dataURL = dataURL = await canvas.toDataURL();
-    }
-    catch (e) {
-        console.log({ url });
-        console.log({ e });
-    }
+  let img = new Image();
+  img.setAttribute("crossorigin", "anonymous");
+  img.src = url;
 
-    //Return
-    return new Promise(resolve => resolve(dataURL));
+  await img.decode()
+
+  return img
+
 }
 
-async function takePhoto() {
+const getImgBase64 = async (imgHTMLElement) => {
 
-  const SERVER_URL = "http://127.0.0.1:7860";
-  const SERVER_API_ENDPOINT = "/sdapi/v1/txt2img";
+  try {
 
-  // const CAMERA_CURRENT_URL = "http://192.168.8.178:8081/101/current";
-  const CAMERA_CURRENT_URL = "/test.jpg";
+    let canvas = document.createElement('CANVAS');
+    canvas.width = 512;
+    canvas.height = 512;
 
-  const prompt = "a group of (esoteric magicians:1.1) on an (exoplanet:1.1) point of view during sunrise, simple outlined illustration, beachy colors, <lora:sd_xl_turbo_lora_v1:1>, (dali style:1.3)";
-  const negative_prompt = "text, bad art, blurry, watermark, person, tripod, letters, ugly, deformed, glasses"
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(imgHTMLElement,
+                  // SOURCE X, Y, WIDTH, HEIGHT
+                  // 512 + 64x2 margin = 640
+                  64, 0, 512, 480,
+                  // DESTINATION X, Y, WIDTH, HEIGHT
+                  // 512 - 480 = 32, remove 16px each side
+                  0, -16, 512, 512
+                 )
 
-  const inputImage = await getImgBase64(CAMERA_CURRENT_URL);
+    const outputElement = document.getElementById("output")
+    outputElement.appendChild(canvas);
+
+    var dataURL = dataURL = await canvas.toDataURL();
+
+  } catch (e) {
+
+    console.log({ url });
+    console.log({ e });
+
+  }
+
+  return new Promise(resolve => resolve(dataURL));
+}
+
+const sdApiRequest = async (inputImgBase64) => {
+
+  const SERVER_API_ENDPOINT = "/sdapi/sdapi/v1/txt2img";
+
+  const prompt = "(esoteric magicians:1.1) on an (exoplanet:1.1) point of view during sunrise, simple outlined illustration, beachy colors, <lora:sd_xl_turbo_lora_v1:1>, (dali style:1.3)";
+  const negative_prompt = "text, bad art, blurry, watermark, person, tripod, letters, ugly, deformed, glasses";
 
   const controlNetParams = [
     {
+      "batch_image_dir": "",
+      "batch_input_gallery": null,
+      "batch_mask_dir": "",
+      "batch_mask_gallery": null,
+      "control_mode": "Balanced",
       "enabled": true,
-      "input_image": inputImage,
-      "model": "controlnet-canny-sdxl-1.0 [7b2ce256]",
+      "guidance_end": 1.0,
+      "guidance_start": 0.0,
+      "hr_option": "Both",
+      "image": inputImgBase64,
+      "mask_image": null,
+      "mask_image_fg": null,
+      "model": "control-lora-canny-rank128 [c910cde9]",
       "module": "canny",
-      "weight": 1.1
+      "pixel_perfect": false,
+      "processor_res": 512,
+      "resize_mode": "Crop and Resize",
+      "save_detected_map": true,
+      "threshold_a": 30,
+      "threshold_b": 158,
+      "use_preview_as_input": false,
+      "weight": 1.2
+    },
+    {
+      "batch_image_dir": "",
+      "batch_input_gallery": null,
+      "batch_mask_dir": "",
+      "batch_mask_gallery": null,
+      "control_mode": "Balanced",
+      "enabled": true,
+      "guidance_end": 1.0,
+      "guidance_start": 0.0,
+      "hr_option": "Both",
+      "image": inputImgBase64,
+      "mask_image": null,
+      "mask_image_fg": null,
+      "model": "thibaud_xl_openpose_256lora [14288071]",
+      "module": "openpose_full",
+      "pixel_perfect": false,
+      "processor_res": 512,
+      "resize_mode": "Crop and Resize",
+      "save_detected_map": true,
+      "threshold_a": 0.5,
+      "threshold_b": 0.5,
+      "use_preview_as_input": false,
+      "weight": 1
+    },
+    {
+      "batch_image_dir": "",
+      "batch_input_gallery": null,
+      "batch_mask_dir": "",
+      "batch_mask_gallery": null,
+      "control_mode": "Balanced",
+      "enabled": true,
+      "generated_image": null,
+      "guidance_end": 1.0,
+      "guidance_start": 0.0,
+      "hr_option": "Both",
+      "image": inputImgBase64,
+      "mask_image": null,
+      "mask_image_fg": null,
+      "model": "instantIDSDXL_ipAdapterInstantId [eb2d3ec0]",
+      "module": "InsightFace (InstantID)",
+      "pixel_perfect": false,
+      "processor_res": 0.5,
+      "resize_mode": "Crop and Resize",
+      "save_detected_map": true,
+      "threshold_a": 0.5,
+      "threshold_b": 0.5,
+      "use_preview_as_input": false,
+      "weight": 1.2
     }
   ];
 
@@ -52,9 +135,9 @@ async function takePhoto() {
     "width": 1024,
     "height": 1024,
 
-    "steps": 4,
+    "steps": 10,
     "sampler_name": "LCM",
-    "cfg_scale": 1.5,
+    "cfg_scale": 1.7,
 
     "override_settings": {
       "sd_model_checkpoint": "sd_xl_base_1.0_0.9vae",
@@ -62,32 +145,204 @@ async function takePhoto() {
     },
 
     "alwayson_scripts": {
-      "controlnet": {
+      "ControlNet": {
         "args": controlNetParams
-      }
+      },
+      "Sampler": {
+        "args": [10, "LCM", "Automatic"]
+      },
     },
 
     "send_images": true,
     "save_images": true,
   };
 
-  fetch(`${SERVER_URL}${SERVER_API_ENDPOINT}`, {
+  // const paramsFromUI = {
+  //   "batch_size": 1,
+  //   "cfg_scale": 1.7,
+  //   "comments": {},
+  //   "denoising_strength": 0.7,
+  //   "disable_extra_networks": false,
+  //   "distilled_cfg_scale": 3.5,
+  //   "do_not_save_grid": false,
+  //   "do_not_save_samples": false,
+  //   "enable_hr": false,
+  //   "height": 1024,
+  //   "hr_cfg": 7,
+  //   "hr_distilled_cfg": 3.5,
+  //   "hr_negative_prompt": "",
+  //   "hr_prompt": "",
+  //   "hr_resize_x": 0,
+  //   "hr_resize_y": 0,
+  //   "hr_scale": 2,
+  //   "hr_second_pass_steps": 0,
+  //   "hr_upscaler": "Latent",
+  //   "n_iter": 1,
+  //   "negative_prompt": "text,bad art,blurry,watermark,person,tripod,letters,ugly,deformed,",
+  //   "override_settings": {},
+  //   "override_settings_restore_afterwards": true,
+  //   "prompt": "a group of (esoteric magicians:1.1) on an (exoplanet:1.1) point of view during sunrise,simple outlined illustration,beachy colors,<lora:sd_xl_turbo_lora_v1:1>,(dali style:1.3),",
+  //   "restore_faces": false,
+  //   "s_churn": 0.0,
+  //   "s_min_uncond": 0.0,
+  //   "s_noise": 1.0,
+  //   "s_tmax": null,
+  //   "s_tmin": 0.0,
+  //   "sampler_name": "LCM",
+  //   "scheduler": "Automatic",
+  //   "script_args": [],
+  //   "script_name": null,
+  //   "seed": -1,
+  //   "seed_enable_extras": true,
+  //   "seed_resize_from_h": -1,
+  //   "seed_resize_from_w": -1,
+  //   "steps": 10,
+  //   "styles": [],
+  //   "subseed": -1,
+  //   "subseed_strength": 0,
+  //   "tiling": false,
+  //   "width": 1024
+  // };
+
+  const response = await fetch(SERVER_API_ENDPOINT, {
     method: "POST",
     body: JSON.stringify(txt2imgParams),
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
   })
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json)
+  const outputJson = await response.json();
+  const base64Img = outputJson.images[0]
 
-    const base64Img = json.images[0]
+  var image = new Image();
+  image.src = `data:image/png;base64,${base64Img}`;
 
-    var image = new Image();
-    image.src = `data:image/png;base64,${base64Img}`;
+  return image;
+}
 
-    document.body.appendChild(image);
-  });
+const displayResult = (imgHTMLElement) => {
+
+  const loadingProgressElement = document.getElementById("loadingProgress")
+  loadingProgressElement.classList.add("d-none")
+
+  const outputElement = document.getElementById("output")
+  outputElement.appendChild(imgHTMLElement);
 
 }
+
+const startCountdown = () => {
+
+  let btnPhoto = document.getElementById("btnPhoto");
+  let btnTxt = document.getElementById("btnTxt");
+  let seconds = 1;
+
+  btnPhoto.disabled = true;
+  btnTxt.innerText = seconds + "...";
+  seconds--;
+
+  let countdown = setInterval(() => {
+
+    if (seconds > 0) {
+
+      btnTxt.innerText = seconds + "...";
+      seconds--;
+
+    } else {
+
+      clearInterval(countdown);
+      btnTxt.innerText = "SMILE!";
+
+      setTimeout(takePhoto, 1000)
+
+    }
+
+  }, 1000);
+}
+
+const nextPhoto = () => {
+
+  let btnPhoto = document.getElementById("btnPhoto");
+  btnPhoto.addEventListener("click", startCountdown)
+
+  let btnTxt = document.getElementById("btnTxt");
+  btnTxt.innerText = "Take Photo!";
+
+  let divStream = document.getElementById("stream");
+  divStream.classList.remove("d-none")
+
+  let divOutput = document.getElementById("output");
+  divOutput.classList.add("d-none")
+
+  while (divOutput.hasChildNodes()) {
+    divOutput.removeChild(divOutput.firstChild);
+  }
+
+}
+
+const takePhoto = async () => {
+
+  const loadingProgressElement = document.getElementById("loadingProgress")
+  loadingProgressElement.classList.remove("d-none");
+
+  let progressStep = 25;
+  let progressInterval = setInterval(() => {
+
+    if (progressStep < 100) {
+
+      loadingProgressElement.firstChild.classList.remove(`w-${progressStep}`)
+      progressStep += 25;
+      loadingProgressElement.firstChild.classList.add(`w-${progressStep}`)
+
+    }
+
+  }, 7000);
+
+  let divStream = document.getElementById("stream");
+  divStream.classList.add("d-none")
+
+  let btnPhoto = document.getElementById("btnPhoto");
+  let btnSpinner = document.getElementById("btnSpinner");
+  let btnStatus = document.getElementById("btnStatus");
+  let btnTxt = document.getElementById("btnTxt");
+
+  btnSpinner.classList.remove("d-none")
+  btnStatus.classList.remove("d-none")
+  btnTxt.classList.add("d-none")
+
+  const CAMERA_CURRENT_URL = "/current";
+
+  const sourceImgHTMLElement = await getImgSource(CAMERA_CURRENT_URL)
+  const inputImgBase64 = await getImgBase64(sourceImgHTMLElement);
+  const outputImgHTMLElement = await sdApiRequest(inputImgBase64);
+
+  displayResult(outputImgHTMLElement);
+  clearInterval(progressInterval);
+
+  // Show output div
+  let divOutput = document.getElementById("output");
+  divOutput.classList.remove("d-none")
+
+  // update button element display
+  btnSpinner.classList.add("d-none")
+  btnStatus.classList.add("d-none")
+  btnTxt.classList.remove("d-none")
+
+  // Update action button for next photo
+  btnPhoto.disabled = false;
+  btnTxt.innerText = "Next Photo!";
+
+  // Update button click event
+  btnPhoto.addEventListener("click", nextPhoto)
+
+}
+
+document.getElementById("btnPhoto").addEventListener("click", startCountdown)
+
+// const test = async () => {
+//
+//   const CAMERA_CURRENT_URL = "/current";
+//
+//   const sourceImgHTMLElement = await getImgSource(CAMERA_CURRENT_URL)
+//   const inputImgBase64 = await getImgBase64(sourceImgHTMLElement);
+// }
+// document.getElementById("btnPhoto").addEventListener("click", test)

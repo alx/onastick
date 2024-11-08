1a14df3557564cecc8c3f3aac6687f4b87fb0080
Alexandre Girard Davila <git@alexgirard.com>
2024-11-08 18:45:18 +0700

    fix: add sleep and return http content

--- server/bin/webcam_capture.sh
+++ server/bin/webcam_capture.sh
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 # Capture webcam image with ffmpeg
 # Stop/start mjpg_stream service before/after capture
-rc-service webcam_stream stop
-ffmpeg -y -f v4l2 -s 960x720 -i /dev/video0 -ss 0:0:2 -frames 1 /media/mmcblk0p2/onastick/capture.jpg
-rc-service webcam_stream start
-cat << EOF
-Content-Type: text/html
-
-<html><head><title>Capture</title><meta charset="UTF-8"></head>
-<body><img src="/capture.jpg"/></body></html>
-EOF
+{
+  rc-service webcam_stream stop
+  sleep .5
+  ffmpeg -y -f v4l2 -s 960x720 -i /dev/video0 -ss 0:0:2 -frames 1 /media/mmcblk0p2/onastick/capture.jpg
+  sleep .5
+  rc-service webcam_stream start
+} &> /dev/null
+echo "HTTP/1.0 200 OK"
+echo "Content-type: text/plain"
+echo ""
+echo $(base64 /media/mmcblk0p2/onastick/capture.jpg)

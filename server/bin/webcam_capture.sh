d8622d2d80a740a73bc3712e496bcb86edff6d1e
Alexandre Girard Davila <git@alexgirard.com>
2024-11-12 14:15:56 +0700

    feat: use monit

--- server/bin/webcam_capture.sh
+++ server/bin/webcam_capture.sh
@@ -3,13 +3,11 @@
 # Capture webcam image with ffmpeg
 # Stop/start mjpg_stream service before/after capture
 {
-  rc-service webcam_stream stop
-  sleep .5
-  ffmpeg -y -f v4l2 -s 960x720 -i /dev/video0 -ss 0:0:2 -frames 1 /media/mmcblk0p2/onastick/capture.jpg
-  sleep .5
-  rc-service webcam_stream start
+  monit stop webcam_stream
+  ffmpeg -y -f v4l2 -s 960x720 -i /dev/video0 -ss 0:0:2 -frames:v 1 /media/mmcblk0p2/onastick/capture_%03d.jpg
+  monit start webcam_stream
 } &> /dev/null
 echo "HTTP/1.0 200 OK"
 echo "Content-type: text/plain"
 echo ""
-echo $(base64 /media/mmcblk0p2/onastick/capture.jpg)
+echo $(base64 /media/mmcblk0p2/onastick/capture_001.jpg)
